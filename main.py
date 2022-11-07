0ca1d6124d88b93a66f150984f008e64a6f83c31
hbombur <andrew-991@yandex.ru>
2022-11-07 14:54:01 +0300

    Add User interface for Timer 2.0.

--- main.py
+++ main.py
@@ -21,21 +21,22 @@ def timer():
     window.setWindowTitle("Таймер 2.0")
     window.setGeometry(scr_shift_x, scr_shift_y, scr_width, scr_height)
 
+    label_greeting = QtWidgets.QLabel(window)
+    label_greeting.setText("Привет, дружище!")
+    label_greeting.move(int(scr_width/2) - int(label_greeting.width()/2), int(scr_height/6))
+    label_greeting.width()
+
     btn_game = QtWidgets.QPushButton(window)
     btn_game.setText("Играть")
-    print(btn_game.width())
-    print(scr_width)
-    print(scr_height)
-    print(int(3.9))
     x = (scr_width/2 - btn_game.width()/2)
-    y = (scr_height/3) - 20
-    btn_game.move(int(x), int(y))
+    y = (scr_height/6)
+    btn_game.move(int(x), int(y * 3))
     btn_game.width()
 
     btn_youtube = QtWidgets.QPushButton(window)
     btn_youtube.setText("Смотреть YouTube")
-    btn_youtube.move(int(scr_width/2) - int(btn_game.width()/2), int(scr_height/3*2) + 20)
-    btn_youtube.width()
+    btn_youtube.move(int(x), int(y * 4))
+    btn_youtube.adjustSize()
     window.show()
     sys.exit(app.exec_())
 
