6b31d9cb690496309d56df6fb8fc784149233acb
hbombur <andrew-991@yandex.ru>
2022-12-20 17:10:41 +0300

    test some varius of code My async.gather() raise except "SystemExit: 0", and i need testing what happend. Well, I found a parameter 'return_exceptions' then skipped this and other exceptions.
    I don't add log about start the program, because it's terrible. I do it not for release, do it for practice.

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 # from UI.UI_timer.timer_ui import init_ui
 # import sys
 import asyncio
+import sys
+
 
 from pathlib import Path
 from timer.timer import decrease_time, timer
 from src.classes import save_data, PlainDataStorage
+from timer_window import test_init
 from UI.UI_timer.timer_ui import init_ui
 
 async def main():
     tasks = [
-        init_ui(),
+        test_init(),
         save_data(PlainDataStorage(Path.cwd() / "testdata.txt"))
 
     ]
